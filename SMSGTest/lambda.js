5585a12a807e0f6f44a38c22924f1c11c20071f7
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-03-26 00:36:27 +0530

    Added SNS

--- SMSGTest/lambda.js
+++ SMSGTest/lambda.js
@@ -1,6 +1,31 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const sns = new AWS.SNS();
+exports.handler = function (event, context, callback) {
 
-	console.log(event);
-	callback(null,'Successfully executed');
+	let receiverNumber = event['receiver'];
+	let sender = event['sender'];
+	let message = event['message'];
+
+	sns.publish({
+		Message: message,
+		MessageAttributes: {
+			'AWS.SNS.SMS.SMSType': {
+				DataType: 'String',
+				StringValue: 'Promotional'
+			},
+			'AWS.SNS.SMS.SenderID': {
+				DataType: 'String',
+				StringValue: sender
+			},
+		},
+		PhoneNumber: receiverNumber
+	}).promise()
+		.then(data => {
+			console.log("Sent SMS to", receiverNumber);
+			callback(null, {...event, status: 'Successful'});
+		})
+		.catch(err => {
+			console.log("Failed to send SMS", err);
+			callback(err, {...event, status: 'Failed'});
+		});
 }
